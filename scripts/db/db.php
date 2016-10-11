75b76a6cce23dc4ad7c7e29dcb449ba1d648d139
Ant'Hannes Phillips <aphil013@ucr.edu>
2016-10-11 08:46:04 +0000

    nav tweaks and db

--- scripts/db/db.php
+++ scripts/db/db.php
@@ -2,7 +2,7 @@
 
      //Connect to the database
     $host = "127.0.0.1";
-    $user = "thedon411";                     //Your Cloud 9 username
+    $user = "aphil013";                     //Your Cloud 9 username
     $pass = "";                                  //Remember, there is NO password by default!
     $db = "clients";                                  //Your database name you want to connect to
     $port = 3306;                                //The port #. It is always 3306
